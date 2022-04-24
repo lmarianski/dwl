bae8e82265c858d8191e69dc387476ba713dfb2b
Łukasz Mariański <lmarianski@protonmail.com>
2022-04-24 13:01:52 +0200

    dwl

--- PKGBUILD
+++ PKGBUILD
@@ -1,6 +1,6 @@
 # Maintainer: Devin J. Pohly <djpohly+arch@gmail.com>
 pkgname=dwl-git
-pkgver=0.2.1.r17.823cefd
+pkgver=0.3.1.r0.b86fcf6
 pkgrel=1
 pkgdesc="Simple, hackable dynamic tiling Wayland compositor (dwm for Wayland)"
 arch=('x86_64')
@@ -13,14 +13,53 @@ provides=("${pkgname%-git}")
 conflicts=("${pkgname%-git}")
 # append #branch=wlroots-next to build against wlroots-git
 source=('git+https://github.com/djpohly/dwl'
-        config.h)
-sha256sums=('SKIP' 'SKIP')
+		'dwl-scratchpad.patch'
+)
+sha256sums=('SKIP'
+            'SKIP')
 
 prepare() {
 	cd "$srcdir/${pkgname%-git}"
 	cp "$srcdir/config.h" config.h
 	# Uncomment to compile with XWayland support
-	#sed -i -e '/-DXWAYLAND/s/^#//' config.mk
+	sed -i -e '/-DXWAYLAND/s/^#//' config.mk
+
+	patch -p1 --input "$srcdir/dwl-scratchpad.patch"
+
+	# This package provides a mechanism to provide a custom config.h. Multiple
+	# configuration states are determined by the presence of two files in
+	# $BUILDDIR:
+	#
+	# config.h  config.def.h  state
+	# ========  ============  =====
+	# absent    absent        Initial state. The user receives a message on how
+	#                         to configure this package.
+	# absent    present       The user was previously made aware of the
+	#                         configuration options and has not made any
+	#                         configuration changes. The package is built using
+	#                         default values.				
+	# present                 The user has supplied his or her configuration. The
+	#                         file will be copied to $srcdir and used during
+	#                         build.
+	#
+	# After this test, config.def.h is copied from $srcdir to $BUILDDIR to
+	# provide an up to date template for the user.
+	echo $srcdir
+
+	echo 
+
+	if [ -e "$BUILDDIR/config.h" ]
+	then
+		cp "$BUILDDIR/config.h" .
+	elif [ ! -e "$BUILDDIR/config.def.h" ]
+	then
+		msg='This package can be configured in config.h. Copy the config.def.h '
+		msg+='that was just placed into the package directory to config.h and '
+		msg+='modify it to change the configuration. Or just leave it alone to '
+		msg+='continue to use default values.'
+		echo "$msg"
+	fi
+	cp config.def.h "$BUILDDIR"
 }
 
 pkgver() {
--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 /* appearance */
 static const int sloppyfocus        = 1;  /* focus follows mouse */
 static const unsigned int borderpx  = 1;  /* border pixel of windows */
+static const int lockfullscreen     = 1;  /* 1 will force focus on the fullscreen window */
 static const float rootcolor[]      = {0.3, 0.3, 0.3, 1.0};
 static const float bordercolor[]    = {0.5, 0.5, 0.5, 1.0};
 static const float focuscolor[]     = {1.0, 0.0, 0.0, 1.0};
@@ -12,8 +13,8 @@ static const Rule rules[] = {
 	/* app_id     title       tags mask     isfloating   monitor */
 	/* examples:
 	{ "Gimp",     NULL,       0,            1,           -1 },
-	{ "firefox",  NULL,       1 << 8,       0,           -1 },
 	*/
+	{ "firefox",  NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -24,16 +25,14 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 };
 
-/* monitors
- * The order in which monitors are defined determines their position.
- * Non-configured monitors are always added to the left. */
+/* monitors */
 static const MonitorRule monrules[] = {
-	/* name       mfact nmaster scale layout       rotate/reflect x y */
+	/* name       mfact nmaster scale layout       rotate/reflect */
 	/* example of a HiDPI laptop monitor:
-	{ "eDP-1",    0.5,  1,      2,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL, 0, 0 },
+	{ "eDP-1",    0.5,  1,      2,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL },
 	*/
 	/* defaults */
-	{ NULL,       0.55, 1,      1,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL, 0, 0 },
+	{ NULL,       0.55, 1,      1,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL },
 };
 
 /* keyboard */
@@ -42,6 +41,8 @@ static const struct xkb_rule_names xkb_rules = {
 	/* example:
 	.options = "ctrl:nocaps",
 	*/
+	.options = "",
+	.layout = "pl"
 };
 
 static const int repeat_rate = 25;
@@ -51,25 +52,41 @@ static const int repeat_delay = 600;
 static const int tap_to_click = 1;
 static const int natural_scrolling = 0;
 
-#define MODKEY WLR_MODIFIER_ALT
+/* If you want to use the windows key change this to WLR_MODIFIER_LOGO */
+#define MODKEY WLR_MODIFIER_LOGO
 #define TAGKEYS(KEY,SKEY,TAG) \
 	{ MODKEY,                    KEY,            view,            {.ui = 1 << TAG} }, \
 	{ MODKEY|WLR_MODIFIER_CTRL,  KEY,            toggleview,      {.ui = 1 << TAG} }, \
 	{ MODKEY|WLR_MODIFIER_SHIFT, SKEY,           tag,             {.ui = 1 << TAG} }, \
 	{ MODKEY|WLR_MODIFIER_CTRL|WLR_MODIFIER_SHIFT,SKEY,toggletag, {.ui = 1 << TAG} }
+#define CHVT(n) { WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT,XKB_KEY_XF86Switch_VT_##n, chvt, {.ui = (n)} }	
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+#define VOLCTRL(amt)   { .v = (const char*[]){ "/usr/bin/pactl", "set-sink-volume", "0", amt, NULL } }
+#define AUDIOCTRL(cmd) { .v = (const char*[]){ "/usr/bin/playerctl", cmd, NULL } }
+#define BRGCTRL(amt)   { .v = (const char*[]){ "/usr/bin/brightnessctl", "set", amt, NULL } }
+
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
+
 /* commands */
 static const char *termcmd[] = { "alacritty", NULL };
 static const char *menucmd[] = { "bemenu-run", NULL };
+// static const char *termcmd[] = { "st", NULL };
+// static const char *menucmd[] = { "dmenu_run", NULL };
+
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "alacritty", "-t", scratchpadname, NULL };
+
+#include <X11/XF86keysym.h>
 
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
 	/* modifier                  key                 function        argument */
-	{ MODKEY,                    XKB_KEY_p,          spawn,          {.v = menucmd} },
+	{ MODKEY,                    XKB_KEY_d,          spawn,          {.v = menucmd} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Return,     spawn,          {.v = termcmd} },
+//	{ MODKEY,                    XKB_KEY_grave,      togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                    XKB_KEY_j,          focusstack,     {.i = +1} },
 	{ MODKEY,                    XKB_KEY_k,          focusstack,     {.i = -1} },
 	{ MODKEY,                    XKB_KEY_i,          incnmaster,     {.i = +1} },
@@ -84,7 +101,7 @@ static const Key keys[] = {
 	{ MODKEY,                    XKB_KEY_m,          setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                    XKB_KEY_space,      setlayout,      {0} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_space,      togglefloating, {0} },
-	{ MODKEY, 					 XKB_KEY_e,    		togglefullscreen, {0} },
+	{ MODKEY,                    XKB_KEY_e,          togglefullscreen, {0} },
 	{ MODKEY,                    XKB_KEY_0,          view,           {.ui = ~0} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_parenright, tag,            {.ui = ~0} },
 	{ MODKEY,                    XKB_KEY_comma,      focusmon,       {.i = WLR_DIRECTION_LEFT} },
@@ -96,15 +113,32 @@ static const Key keys[] = {
 	TAGKEYS(          XKB_KEY_3, XKB_KEY_numbersign,                 2),
 	TAGKEYS(          XKB_KEY_4, XKB_KEY_dollar,                     3),
 	TAGKEYS(          XKB_KEY_5, XKB_KEY_percent,                    4),
-	TAGKEYS(          XKB_KEY_6, XKB_KEY_caret,                      5),
+	TAGKEYS(          XKB_KEY_6, XKB_KEY_asciicircum,                5),
 	TAGKEYS(          XKB_KEY_7, XKB_KEY_ampersand,                  6),
 	TAGKEYS(          XKB_KEY_8, XKB_KEY_asterisk,                   7),
 	TAGKEYS(          XKB_KEY_9, XKB_KEY_parenleft,                  8),
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Q,          quit,           {0} },
 
+	/* MEDIA */
+	{ 0,                         XF86XK_AudioMute,   spawn,          {.v = mutevol } },
+	{ 0,                         XF86XK_AudioLowerVolume,  spawn,    VOLCTRL("-5%") },
+	{ 0,                         XF86XK_AudioRaiseVolume,  spawn,    VOLCTRL("+5%") },
+	{ WLR_MODIFIER_SHIFT,        XF86XK_AudioLowerVolume,  spawn,    VOLCTRL("-1%") },
+	{ WLR_MODIFIER_SHIFT,        XF86XK_AudioRaiseVolume,  spawn,    VOLCTRL("+1%") },
+	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_F1,               spawn,    AUDIOCTRL("previous") },
+	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_F2,               spawn,    AUDIOCTRL("next") },
+	// { MODKEY,                 XK_Escape,                spawn,    AUDIOCTRL("play-pause") },
+	{ MODKEY,                    XKB_KEY_Escape,           spawn,    SHCMD("~/scripts/media-toggle.sh") },
+	/* FN */
+	{ MODKEY,                    XKB_KEY_l,                spawn,    SHCMD("xautolock -locknow") },
+	{ 0,                         XF86XK_MonBrightnessDown, spawn,    BRGCTRL("10%-") },
+	{ WLR_MODIFIER_SHIFT,        XF86XK_MonBrightnessDown, spawn,    BRGCTRL("5%-") },
+	{ 0,                         XF86XK_MonBrightnessUp,   spawn,    BRGCTRL("+10%") },
+	{ WLR_MODIFIER_SHIFT,        XF86XK_MonBrightnessUp,   spawn,    BRGCTRL("+5%") },
+	{ 0,                         XF86XK_Calculator,        spawn,    SHCMD("$CALCULATOR") },
+
 	/* Ctrl-Alt-Backspace and Ctrl-Alt-Fx used to be handled by X server */
 	{ WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT,XKB_KEY_Terminate_Server, quit, {0} },
-#define CHVT(n) { WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT,XKB_KEY_XF86Switch_VT_##n, chvt, {.ui = (n)} }
 	CHVT(1), CHVT(2), CHVT(3), CHVT(4), CHVT(5), CHVT(6),
 	CHVT(7), CHVT(8), CHVT(9), CHVT(10), CHVT(11), CHVT(12),
 };
